38bb99957d70113044d27836cb37158a88f32ebd
Ewen Diler <ewendiler@MacBook-Pro-de-Ewen.local>
2026-01-15 09:32:59 +0100

    Exam

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -4,14 +4,18 @@ import { getRecipeById } from "../controllers/recipesController.js"
 import { createRecipe } from "../controllers/recipesController.js"
 import { updateRecipe } from "../controllers/recipesController.js"
 import { deleteRecipe } from "../controllers/recipesController.js"
+import { searchRecipes } from "../controllers/recipesController.js"
 
 
 const router = Router()
 
 router.get("/", getRecipes)
+router.get('/search', searchRecipes)
 router.get('/:id', getRecipeById)
 router.post('/', createRecipe)
 router.put('/:id', updateRecipe)
 router.delete('/:id', deleteRecipe)
+router.delete('/:id', searchRecipes)
+router.get('/:id', getRecipeById)
 
 export default router
